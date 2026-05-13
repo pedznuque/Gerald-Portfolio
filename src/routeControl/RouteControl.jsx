5d85e5dd17fadf56f8eb2b6cbbcbfd054888e755
Gerald <geraldyup9@gmail.com>
2026-05-14 02:16:01 +0800

    Update portfolio

--- src/routeControl/RouteControl.jsx
+++ src/routeControl/RouteControl.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
 import Project from "../pages/project/Project";
@@ -9,7 +9,8 @@ const RouteControl = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="home" replace/>}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Project />} />
         <Route path="/blog" element={<Blog />} />
